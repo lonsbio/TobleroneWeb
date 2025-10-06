407102237e6318c56ee4e93f4bf9c1dc1acbf3b2
lonsbio <andrew.lonsdale@lonsbio.com.au>
2025-10-06 06:00:57 +0000

    fix test aioli in js

--- main.js
+++ main.js
@@ -6,7 +6,16 @@ async function boot() {
     console.warn("Page is not crossOriginIsolated; threads may be unavailable.");
   }
 
-  const a = await new Aioli();
+
+
+const a = await new Aioli([{tool: "tinyt",
+   // version: "0.0.6",
+   // program: "fastqe",         // Optional: sub-tool name; not needed for most tools (default: same as tool name)
+    urlPrefix: "https://lonsbio.github.io/TobleroneWeb/",  // Optional: custom path to .wasm assets (default: biowasm CDN)
+ //   loading: "eager",         // Optional: if set to "lazy", only downloads WebAssembly modules when needed, not at initialization (default: eager)
+ //   reinit: false,           // Optional: if set to true, will reinitialize module after each invocation; not needed for most tools
+}]);
+
   // e.g., list tools or run a simple command if configured in your setup
   // const res = await a.exec("echo Hello from Aioli");
   // document.getElementById("app").textContent = res.stdout || "Ready";
