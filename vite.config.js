bacc4e584d3333c8464e4661e688d179b92fdc79
lonsbio <andrew.lonsdale@lonsbio.com.au>
2025-10-06 05:14:12 +0000

    local aioli test

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+// If deploying to https://<user>.github.io/<REPO>/ set base = "/<REPO>/"
+// If using a custom domain at the root, set base = "/"
+const base = process.env.GITHUB_REPOSITORY?.split("/")[1]
+  ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}/`
+  : "/";
+
+export default defineConfig({
+  base,
+  build: { outDir: "dist" },
+});
