121e229ac299c727aaca2611ec207604e69e8cd4
lonsbio <andrew.lonsdale@lonsbio.com.au>
2025-10-08 02:33:42 +0000

    try without test Aili code

--- main.js
+++ main.js
@@ -7,22 +7,6 @@ async function boot() {
   }
 }
 
-window.AioliReady = (async () => {
-  const CLI = new Aioli([{tool: "tinyt",
-   // version: "0.0.6",
-   // program: "fastqe",         // Optional: sub-tool name; not needed for most tools (default: same as tool name)
-    urlPrefix: "https://lonsbio.github.io/TobleroneWeb/",  // Optional: custom path to .wasm assets (default: biowasm CDN)
- //   loading: "eager",         // Optional: if set to "lazy", only downloads WebAssembly modules when needed, not at initialization (default: eager)
- //   reinit: false,           // Optional: if set to true, will reinitialize module after each invocation; not needed for most tools
-}]);
-  output_help = await CLI.exec("tinyt --help");
-  document.getElementById("output_help").innerHTML = output_help;
-
-
-  return CLI;
-})();
-
-
 boot().catch(err => {
   console.error(err);
   document.getElementById("app").textContent = "Error: " + err;
